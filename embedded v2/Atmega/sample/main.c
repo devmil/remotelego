825c5ea9b8c24614882892662995e4f2ddd5b551
Michael Lamers <devmil@posteo.de>
2016-04-27 00:19:17 +0200

    fixed some bugs added reason of reset/power on output added delay for reading the serial input (stabilizes the system, further investigation needed) added watchdog added support for more commands in one telegram

--- embedded v2/Atmega/sample/main.c
+++ embedded v2/Atmega/sample/main.c
@@ -1,7 +1,9 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>	// include interrupt support
+#include <avr/wdt.h>
 #include <util/delay.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "uart.h"
 
@@ -18,18 +20,20 @@
 #define MAIN_MOTOR_PRESCALER_BITS	(1<<CS10)
 #define MAIN_MOTOR_TIMER_TICKS		255
 
-#define UART_BAUDRATE				19200
+#define UART_BAUDRATE				38400
+
+#define RECEIVER_BUFF_SIZE 128
 
 typedef struct {
-	float milliseconds;
-	uint8_t seconds;
-	uint8_t minutes;
-	uint8_t hours;
+	double milliseconds;
+	uint16_t seconds;
+	uint16_t minutes;
+	uint16_t hours;
 	uint64_t days;
 } Duration;
 
 typedef struct {
-	float tickDurationMs;
+	double tickDurationMs;
 	Duration duration;
 } Clock;
 
@@ -46,8 +50,22 @@ typedef struct {
 	volatile uint16_t* timerCounterRegister;
 } Motor;
 
+void uart_puti(uint32_t integer) {
+	char buff[32];
+	buff[31] = '\0';
+	ltoa(integer, buff, 10);
+	uart_puts(buff);
+}
+
 void Clock_init(Clock* clock, float tickDurationMs) {
 	clock->tickDurationMs = tickDurationMs;
+	Duration initDuration;
+	initDuration.milliseconds = 0;
+	initDuration.seconds = 0;
+	initDuration.minutes = 0;
+	initDuration.hours = 0;
+	initDuration.days = 0;
+	clock->duration = initDuration;
 }
 
 void Clock_tick(Clock* clock) {
@@ -55,7 +73,7 @@ void Clock_tick(Clock* clock) {
 	duration.milliseconds += clock->tickDurationMs;
 	if(duration.milliseconds >= 1000) {
 		duration.seconds++;
-		duration.milliseconds -= 1000;
+		duration.milliseconds -= 1000.0d;
 		if(duration.seconds >= 60) {
 			duration.minutes++;
 			duration.seconds -= 60;
@@ -119,6 +137,12 @@ void Motor_setDirection(Motor* motor, uint8_t direction) {
 }
 
 void Motor_setSpeedPercent(Motor* motor, float percent) {
+	if(percent > 100) {
+		percent = 100;
+	}
+	if(percent < 0) {
+		percent = 0;
+	}
 	if(motor->isInverted) {
 		percent = 100 - percent;
 	}
@@ -127,6 +151,12 @@ void Motor_setSpeedPercent(Motor* motor, float percent) {
 }
 
 void Servo_setPercent(float percent) {
+	if(percent > 100) {
+		percent = 100;
+	}
+	if(percent < 0) {
+		percent = 0;
+	}
 	float servoMs = SERVO_MIN_MS + ((SERVO_MAX_MS - SERVO_MIN_MS) * percent) / 100;
 	float servoTicks = servoMs / SERVO_TIMER_MS_PER_TICK;
 	OCR0 = 255 - (int8_t)servoTicks;
@@ -181,75 +211,140 @@ float s_test_motor_direction = 1;
 
 
 void nextTestStep() {
-	PORTD ^= (1<<PD7); 					// toggle PD7
-		
-	if(!s_test_isWaiting && s_test_servo_percent == 50) {
-		if(s_test_waitingCycles == 0) {
-			s_test_isWaiting = 1;
-			s_test_waitingCycles = 6;
-		}
-	}
-	if(s_test_waitingCycles == 0) {
-		s_test_isWaiting = 0;
-		s_test_servo_percent += s_test_direction * 10;
-		if(s_test_servo_percent > 100) {
-			s_test_direction *= -1;
-			s_test_servo_percent = 100;
-		} else if(s_test_servo_percent < 0) {
-			s_test_direction *= -1;
-			s_test_servo_percent = 0;
-		}
-		Servo_setPercent(s_test_servo_percent);
-	} else {
-		s_test_waitingCycles--;
-	}
+	PORTD ^= (1<<PD7); 					// toggle PD7	
+}
 
-	s_test_motor_percent += s_test_motor_percentageDirection * 10;
-	if(s_test_motor_percent > 100) {
-		s_test_motor_percent = 100;
-		s_test_motor_percentageDirection *= -1;
-	} else if(s_test_motor_percent < -100) {
-		s_test_motor_percent = -100;
-		s_test_motor_percentageDirection *= -1;
-	}
+char s_uartRcvBuffer[RECEIVER_BUFF_SIZE + 1];
+uint8_t s_uartRcvBufferPos = 0;
+uint8_t s_backslashReceived = 0;
+uint8_t s_inCommand = 0;
 
-	Motor_setDirection(&s_mainMotor, s_test_motor_direction);
-	if(s_test_motor_percent < 0) {
-		Motor_setSpeedPercent(&s_mainMotor, -1 * s_test_motor_percent);			
-	} else {
-		Motor_setSpeedPercent(&s_mainMotor, s_test_motor_percent);			
+uint8_t handleCommand(char* command, char* value) {
+	uint8_t result = 0;
+	if(strcmp(command, "ms") == 0) {
+		double doubleVal = atof(value);
+		Motor_setSpeedPercent(&s_mainMotor, (float)doubleVal);
+		result = 1;
+	} else if(strcmp(command, "md") == 0) {
+		int intVal = atoi(value);
+		Motor_setDirection(&s_mainMotor, intVal != 0 ? 1 : 0);
+		result = 1;
+	} else if(strcmp(command, "sp") == 0) {
+		int intVal = atoi(value);
+		Servo_setPercent(intVal);
+		result = 1;
 	}
-	if(s_test_motor_percent == 0) {
-		if(s_test_motor_direction == 0) {
-			s_test_motor_direction = 1;
+	return result;
+}
+
+void handleCommands() {
+	uint8_t inVal = 0;
+	char commandBuff[20] = "";
+	char valBuff[20] = "";
+	uint8_t numOfCommands = 0;
+	for(int i=0; i<s_uartRcvBufferPos; i++) {
+		if(s_uartRcvBuffer[i] == '=') {
+			inVal = 1;
+			continue;			
+		}
+		else if(s_uartRcvBuffer[i] == '|') {
+			if(handleCommand(commandBuff, valBuff)) {
+				numOfCommands++;
+			}
+			commandBuff[0] = '\0';
+			valBuff[0] = '\0';
+			inVal = 0;
+			continue;
+		}
+		else if(inVal) {
+			uint16_t idx = strlen(valBuff);
+			valBuff[idx] =  s_uartRcvBuffer[i];
+			valBuff[idx + 1] = '\0';
 		} else {
-			s_test_motor_direction = 0;
+			uint16_t idx = strlen(commandBuff);
+			commandBuff[idx] =  s_uartRcvBuffer[i];
+			commandBuff[idx + 1] = '\0';
 		}
 	}
-		Duration duration = Clock_getDuration(&s_clock);
+	if(handleCommand(commandBuff, valBuff)) {
+		numOfCommands++;
+	}
+	uart_puts("\\r");
+	uart_puts("nc=");
+	char buff[12];
+	buff[11] = '\0';
+	itoa(numOfCommands, buff, 10);
+	uart_puts(buff);
+	uart_puts("/\r\n");
 	
-	char buffMinutes[16];
-	itoa(duration.minutes, buffMinutes, 10);
-	uart_puts(buffMinutes);
-	uart_puts("m: ");
-	char buffSeconds[16];
-	itoa(duration.seconds, buffSeconds, 10);
-	uart_puts(buffSeconds);
-	uart_puts("s ");
-	char buff[10];
-	itoa((int)s_test_motor_percent, buff, 10);
-	uart_puts("Motor: ");
-	uart_puts(buff);
-	uart_puts("%");
-	uart_puts(", Servo: ");
-	itoa((int)s_test_servo_percent, buff, 10);
-	uart_puts(buff);
-	uart_puts("%");
-	uart_puts("\r\n");	
+	s_inCommand = 0;
+	s_uartRcvBufferPos = 0;
+	
+}
+
+void cancelCommand() {
+	s_inCommand = 0;
+	s_uartRcvBufferPos = 0;
+}
+
+void handlePotentialCommand(char c) {
+	if(!s_backslashReceived 
+		&& !s_inCommand
+		&& s_uartRcvBufferPos == 0) {
+		//wait for the beginning
+		if(c != '\\') {
+			return;
+		}
+		s_backslashReceived = 1;
+		return;
+	}
+	if(s_backslashReceived) {
+		s_backslashReceived = 0;
+		if(c != 'c' && c != 'C') {
+			return;
+		}
+		s_inCommand = 1;
+		return;
+	}
+	if(s_inCommand) {
+		if(c == '/') {
+			handleCommands();
+			return;
+		}
+		s_uartRcvBuffer[s_uartRcvBufferPos] = c;
+		s_uartRcvBufferPos++;
+		if(s_uartRcvBufferPos >= RECEIVER_BUFF_SIZE) {
+			cancelCommand();
+		}
+	}	
+}
+
+void handleUart() {
+	uint8_t resultErrorCode = 0;
+	do {
+		cli();
+		uint16_t getResult = uart_getc();
+		sei();
+		uint8_t c = (uint8_t)getResult;
+		resultErrorCode = (uint8_t)(getResult >> 8);
+		
+		if (resultErrorCode == 0) {
+			handlePotentialCommand(c);
+		}
+	} while(resultErrorCode == 0);
 }
 
 int main(void)
-{	
+{
+	char* reason="No known reason\r\n";	
+	
+	if(MCUCSR & (1<<PORF )) reason = "Power-on reset.\r\n";
+	if(MCUCSR & (1<<EXTRF)) reason = "External reset!\r\n";
+	if(MCUCSR & (1<<BORF )) reason = "Brownout reset!\r\n";
+	if(MCUCSR & (1<<WDRF )) reason = "Watchdog reset!\r\n";
+	if(MCUCSR & (1<<JTRF )) reason = "JTAG reset!\r\n";
+	
+	MCUCSR = 0;
 	uart_init(UART_BAUD_SELECT(UART_BAUDRATE, F_CPU));
 	DDRD |= (1<<PD7); 						// PD7 is output
 	DDRC |= (1<<PC0); 						// PD6 is output
@@ -267,21 +362,40 @@ int main(void)
 
 	sei();
 	
+	uart_puts(reason);
+	uart_puts("Motor control ready\r\n");
+	
 	uint16_t delay_ms = 500;
 	uint16_t nextMilliseconds = 0;
+	uint8_t lastMinute = 0;
+
+	wdt_enable(WDTO_120MS);
 
 	while (42)
 	{
+		handleUart();
 		Duration duration = Clock_getDuration(&s_clock);
-		uint16_t totalMilliseconds = duration.seconds * 1000 + (uint16_t)duration.milliseconds;
 
-		if(totalMilliseconds > nextMilliseconds) {
-			nextTestStep();
-			nextMilliseconds += delay_ms;
-		} else {
-			_delay_ms(10);
+		uint32_t totalMillisecondsThisMinute = (uint32_t)duration.milliseconds;
+		totalMillisecondsThisMinute += (uint32_t)duration.seconds * (uint32_t)1000ul;
+		
+		if(lastMinute != duration.minutes) {
+			lastMinute = duration.minutes;
+			nextMilliseconds -= 60ul * 1000ul;
 		}
 
+		if(totalMillisecondsThisMinute > nextMilliseconds) {
+			nextTestStep();
+			nextMilliseconds += delay_ms;
+		}
+		_delay_ms(10);
+		wdt_reset();
 	}
 	return 0; 								// this line should never be reached
+}
+
+ISR(BADISR_vect)
+{
+
+    for (;;) uart_puts("!");
 }
