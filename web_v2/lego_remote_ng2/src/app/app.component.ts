744852b3b80a03f496f379f4cc6e415bb994bc43
fexdev <florian.wachs@live.de>
2016-06-18 12:52:25 +0200

    basic layout

--- web_v2/lego_remote_ng2/src/app/app.component.ts
+++ web_v2/lego_remote_ng2/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
+import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
+import { MD_TOOLBAR_DIRECTIVES } from '@angular2-material/toolbar';
+import { MD_SLIDE_TOGGLE_DIRECTIVES } from '@angular2-material/slide-toggle';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [MD_TOOLBAR_DIRECTIVES, MD_BUTTON_DIRECTIVES, MD_SLIDE_TOGGLE_DIRECTIVES ]
 })
 export class AppComponent {
   title = 'app works!';
