9169ca33ea144445e5b03f453b90e5e114fb7d62
Michael Lamers <devmil@posteo.de>
2016-08-03 19:30:13 +0200

    Android app: icon for the settings button

--- android/app/src/main/java/de/devmil/legocontrol/ControlActivity.java
+++ android/app/src/main/java/de/devmil/legocontrol/ControlActivity.java
@@ -5,25 +5,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatCheckBox;
-import android.support.v7.widget.ListViewCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.ScrollView;
 import android.widget.Switch;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 import java.util.List;
 import java.util.Locale;
@@ -66,7 +60,7 @@ public class ControlActivity extends AppCompatActivity implements ICarHandlerSta
     private RadioButton mrbBlinkR;
     private RadioButton mrbBlinkLR;
 
-    private Button btnSettings;
+    private ImageButton btnSettings;
 
     private View vSettings;
 
@@ -103,7 +97,7 @@ public class ControlActivity extends AppCompatActivity implements ICarHandlerSta
         mrbBlinkR = (RadioButton)findViewById(R.id.activity_control_rb_blink_r);
         mrbBlinkLR = (RadioButton)findViewById(R.id.activity_control_rb_blink_lr);
 
-        btnSettings = (Button)findViewById(R.id.activity_control_ll_settings_btn);
+        btnSettings = (ImageButton)findViewById(R.id.activity_control_ll_settings_btn);
 
         vSettings = findViewById(R.id.activity_control_Settings);
 
